0c018f7233edf01be60ec8b14c98728d484d934e
Lei_tiger <leifu@oregonstate.edu>
2020-01-23 19:51:55 -0800

    Updates

---
+++ precache-manifest.1e51cf6f53d2e27e32052c0c9f4f7ac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe0c207f1723b61d34cec76be36ea6f",
+    "url": "/newrobofriends/index.html"
+  },
+  {
+    "revision": "42e8b8f7801a142e16a9",
+    "url": "/newrobofriends/static/css/main.caed92ea.chunk.css"
+  },
+  {
+    "revision": "2ff4bc0532ffb5c5102d",
+    "url": "/newrobofriends/static/js/2.0388415b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newrobofriends/static/js/2.0388415b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42e8b8f7801a142e16a9",
+    "url": "/newrobofriends/static/js/main.a46b8692.chunk.js"
+  },
+  {
+    "revision": "caa01fa5ea51d25a27b4",
+    "url": "/newrobofriends/static/js/runtime-main.22dfbc53.js"
+  }
+]);
