0b57596afa079efb154db5f33143c774d18d3684
Lei_tiger <leifu@oregonstate.edu>
2020-02-07 15:06:03 -0800

    pwa

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import styles from './card.module.scss';
 const Card = ({ name, email, id }) => {
     return (
         <div className={styles.Card}>
-            <img alt= "robots" width="200" height="200" src={`https://robohash.org/${id}?size=200x200`} />
+            <img alt= "robots" width="200" height="200" src={`https://robohash.org/${id}?size=300x300`} />
             <div>
                 <h2> {name} </h2>
                 <h3> {email} </h3>
